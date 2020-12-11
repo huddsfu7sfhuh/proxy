a267992c39b89ced6d26deed90acf781ff4bb3d6
huddsfu7sfhuh <43814931+huddsfu7sfhuh@users.noreply.github.com>
2020-12-12 02:17:28 +0300

    Add files via upload

---
+++ auproximity-webui/src/models/ClientSocketEvents.ts
@@ -0,0 +1,14 @@
+enum ClientSocketEvents {
+    SetUuid = 'setuuid',
+    JoinRoom = 'joinroom',
+    Disconnect = 'disconnect',
+    Error = 'error',
+    SetAllClients = 'setallclients',
+    AddClient = 'addclient',
+    RemoveClient = 'removeclient',
+    SetMap = 'setmap',
+    SetPose = 'setpose',
+    SetGroup = 'setgroup',
+}
+
+export default ClientSocketEvents
